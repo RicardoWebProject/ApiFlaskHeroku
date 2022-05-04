8ecf540638247dcf6d0cb9cd3702b1f4a4e3bee4
Ricardo Fuentes <luisrijhn09@gmail.com>
2022-05-04 16:53:33 -0600

    cambiado estado DEBUG a false

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from resources.item import Item, ItemList
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
