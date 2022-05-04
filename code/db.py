6f5bd341c401eb2f2b8128eae0486b4059bfdc2e
Ricardo Fuentes <luisrijhn09@gmail.com>
2022-05-04 11:24:47 -0600

    Objeto de base de datos

---
+++ code/db.py
@@ -0,0 +1,6 @@
+from flask_sqlalchemy import SQLAlchemy
+
+#El Objeto con SQLAlchemy esencialmente enlazará la aplicación Flask
+#Y se verá en todos los objetos que le indiquemos, y permitirá mapear esos objetos a filas en una base de datos.
+db = SQLAlchemy()
+
