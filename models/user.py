da4afb34728c19ed73db2c5f45640f490801844d
Ricardo Fuentes <luisrijhn09@gmail.com>
2022-05-06 15:22:16 -0600

    Agregado método para eliminar desde base de datos

--- models/user.py
+++ models/user.py
@@ -19,6 +19,9 @@ class UserModel(db.Model):
         db.session.add(self)
         db.session.commit()
     
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
     
     @classmethod
     def find_by_username(cls, username):
