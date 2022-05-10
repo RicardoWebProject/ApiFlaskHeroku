e2415996077f34a12cf48e5a892a601b605cfd5e
Ricardo Fuentes <luisrijhn09@gmail.com>
2022-05-10 15:32:35 -0600

    Creados los schemas para item, store y user, similares a los serializadores en Django

---
+++ schemas/user.py
@@ -0,0 +1,21 @@
+# from marshmallow import Schema, fields
+
+from ma import ma
+from models.user import UserModel
+
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = UserModel
+        load_only = ("password",)
+        dump_only = ("id",)
+        load_instance = True
+
+# class UserSchema(Schema):
+#     class Meta:
+#         load_only = ('password',)   #Deberá de ser una tupla
+#         dump_only = ('id',)
+    
+#     id = fields.Int()
+#     username = fields.Str(required=True)
+#     password = fields.Str(required=True)
